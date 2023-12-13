0013b9d50f08b3a690efbbd5c52f0e5c085183f5
mothana404 <https://github.com/mothana404>
2023-12-13 11:31:46 +0300

    now

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -2,10 +2,12 @@ import express from "express";
 import router from './routes/homepageRoutes';
 import sequelize from './config/sequelize';
 import {StudentMap} from './models/userModel';
+import cors from 'cors';
 
 const app: express.Express = express();
 const port: number = 8000;
 app.use(express.json());
+app.use(cors());
 
 StudentMap(sequelize);
 app.use(router);
